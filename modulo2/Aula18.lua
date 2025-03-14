26b1d14fcb31f363a9326ca71ce8d85300709d47
Nathan Scremin <nathanscremin.dev@gmail.com>
2025-03-14 09:53:23 -0300

    Delete modulo2/Aula18.lua

--- modulo2/Aula18.lua
+++
@@ -1,9 +0,0 @@
---Biblioteca OS
-local data = os.date("%d/%m/%Y %H:%M:%S")
---print(os.clock())
---print(os.time())
-print(data)
---os.execute("mkdir pasta")
---os.remove("text.txt")
---os.rename("Teste.txt","Novo.txt")
-os.exit()
